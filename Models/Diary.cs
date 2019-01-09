73bbf2d03d44afdb8529740053940373a1eef054
VahoninMikhail <31822003+VahoninMikhail@users.noreply.github.com>
2019-01-09 19:40:50 +0400

    Add files via upload

---
+++ Models/Diary.cs
@@ -0,0 +1,25 @@
+﻿using Schoolboy_diary.Service;
+
+namespace Schoolboy_diary.Models
+{
+    public class Diary
+    {
+        public int Id { get; set; }
+
+        public string Date { get; set; }
+
+        public int NumLesson { get; set; }
+
+        public string NameLesson { get; set; }
+
+        public int Mark { get; set; }
+
+        public string Homework { get; set; }
+
+        public string Note { get; set; }
+
+        public int? SchoolId { get; set; }
+
+      //  public School School { get; set; }
+    }
+}
---
+++ Models/School.cs
@@ -0,0 +1,19 @@
+﻿using Schoolboy_diary.Service;
+
+namespace Schoolboy_diary.Models
+{
+    public class School
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Grade { get; set; }
+
+    /*    public ICollection<Diary> Diaries { get; set; }
+        public School()
+        {
+            Diaries = new List<Diary>();
+        }*/
+    }
+}
